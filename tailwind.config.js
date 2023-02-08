50263bf3d562acae8d517ba45d40edf0ab3f98a8
Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2023-02-09 02:33:04 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'btn-blue': '#8CF2F2',
+        'btn-sea': "#90ABEF" ,
+        'btn-pink': "#EA88E6" ,
+      },
+    }
+  },
+  plugins: [],
+}
